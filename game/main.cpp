4641d80163e5cc5e5fe5d8b25e54274d58e28fe6
realblobii <realblobii@proton.me>
2025-12-01 16:59:40 +0800

    removed demo code from main.cpp as it will error due to lack of textures or object index

--- game/main.cpp
+++ game/main.cpp
@@ -7,13 +7,10 @@ int main(int argc, const char *argv[])
 {
   engine = new Engine{};
   engine->Init("New Game", 1920, 1080, true);
-  engine->loadTileMap("assets/grass_grid.json",64,64);
-  engine->objMgr->instantiate("tile", "pTest", 15,15,1);
 
   while (engine->running())
   {
     
-  
     engine->handleEvents();
     engine->update();
     engine->render();
